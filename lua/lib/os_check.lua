74b4e6dd69a87a9cf317e10ecd4c5bd1014a5c0e
2pac <tarasyarema@pm.me>
2020-09-28 21:04:13 +0200

    changes

--- lua/lib/os_check.lua
+++ lua/lib/os_check.lua
@@ -1,7 +1,7 @@
 local os_check = {}
 
 os_check.get = function()
-  local separator = package.config:sub(1,1) 
+  local separator = package.config:sub(1,1)
   if separator == "\\" then
       return "win"
   elseif separator == "/" then
